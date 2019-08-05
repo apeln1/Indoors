4ac3f93e59eec6dbbc87a9c1f190a9e3d6e708f4
apeln1 <alexander.plsk+github@gmail.com>
2019-08-05 10:09:43 +0000

    subscribers added

--- route_planning/src/local_mission_planner.py
+++ route_planning/src/local_mission_planner.py
@@ -1,12 +1,44 @@
 #!/usr/bin/python3
 import rospy
 from std_msgs.msg import String
+from nav_msgs.msg import OccupancyGrid
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import BatteryState
+from trajectory_msgs.msg import JointTrajectory
 from LocalMissionPlanner import LocalMissionPlanner
 from EnvSim import Env
 from Grid import Grid
 
+
+occupancyGrid = OccupancyGrid()
+odometry = Odometry()
+jointTrajectory = JointTrajectory()
+
+
+# callbacks
+def occupancyGrid_cb(data):
+    global occupancyGrid
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.header.seq)
+    occupancyGrid = data
+
+def odometry_cb(data):
+    global odometry
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.header.seq)
+    odometry = data
+
+def joint_trajectory_cb(data):
+    global jointTrajectory
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.header.seq)
+    jointTrajectory = data
+
+# subscribers
+rospy.Subscriber("/route_planner/occupancy_grid", OccupancyGrid, occupancyGrid_cb)
+rospy.Subscriber("/route_planner/odometry", Odometry, odometry_cb)
+rospy.Subscriber("/route_planner/joint_trajectory", JointTrajectory, joint_trajectory_cb)
+
+
 pub = rospy.Publisher('topic_name', String, queue_size=10)
-rospy.init_node('node_name')
+rospy.init_node('local_mission_planner')
 r = rospy.Rate(10) # 10hz
 
 
