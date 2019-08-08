3345e0e73bb4a074004f449d802e027ac2fddf16
apeln1 <alexander.plsk+github@gmail.com>
2019-08-08 13:36:09 +0000

    arguments to launch file example

--- sandbox/test1.cpp
+++ sandbox/test1.cpp
@@ -2,8 +2,19 @@
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "test1");
-  ros::NodeHandle nh;
 
-  ROS_INFO("Hello world!");
+  ROS_INFO("argc = ! %s ",argv[0]);
+  if(argc>1)
+  {
+     ROS_INFO("-------------------Hello world! %d ",atoi(argv[1])+1);
+  }
+
+
+   ros::init(argc, argv, "test1");
+   ros::NodeHandle nh;
+
+   ros::Rate rate(20.0);
+
+   ROS_INFO("Hello world!");
+
 }
